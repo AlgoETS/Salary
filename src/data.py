4e475f68d3de4049be6f96af8cc8e570ffc26b41
Antoine Boucher <antoine.bou13@gmail.com>
2023-02-25 21:01:36 +0000

    start clean up

---
+++ src/data.py
@@ -0,0 +1,157 @@
+import pickle
+import pandas as pd
+
+
+def reaf_pickle(path):
+    with open(path, "rb") as f:
+        data = pickle.load(f)
+    return data
+
+
+def save_pickle(data, path):
+    with open(path, "wb") as f:
+        pickle.dump(data, f)
+
+
+def load_data(path):
+    data = reaf_pickle(path)
+
+
+def load_dataframe(data):
+    df = pd.DataFrame(data)
+
+
+def save_dataframe(df, path):
+    df.to_pickle(path)
+
+
+def highest_paying_job():
+    # Split the salary ranges into minimum and maximum salaries
+    df[["entry_min", "entry_max"]] = (
+        df["entry"].str.split("-", expand=True).astype(float)
+    )
+    df[["mid_min", "mid_max"]] = df["mid"].str.split("-", expand=True).astype(float)
+    df[["senior_min", "senior_max"]] = (
+        df["senior"].str.split("-", expand=True).astype(float)
+    )
+
+    # Create a new column with the maximum salary for each job
+    df["max_salary"] = df[["entry_max", "mid_max", "senior_max"]].max(axis=1)
+    # calculate the mean for each level
+    df["entry_mean"] = df[["entry_min", "entry_max"]].mean(axis=1)
+    df["mid_mean"] = df[["mid_min", "mid_max"]].mean(axis=1)
+    df["senior_mean"] = df[["senior_min", "senior_max"]].mean(axis=1)
+
+    # Sort the DataFrame by the maximum salary in descending order
+    df_sorted = df.sort_values("max_salary", ascending=False)
+
+    # Get the job that pays the most
+    job_highest_paid = df_sorted["job"].iloc[0]
+
+    print(f"The job that pays the most is {job_highest_paid}")
+
+    return df_sorted.head()
+
+
+def highest_paying_engineer(df_sorted):
+    top_engineers_jobs = df_sorted[
+        df_sorted["job"].str.contains("engineer", case=False)
+    ]
+    print(f"The job that pays the most is {top_engineers_job}")
+    return top_engineers_jobs.head(30)
+
+
+def lowest_paying_job(df_sorted):
+    worst_engineers_jobs = df_sorted.sort_values("max_salary", ascending=True)
+    top30 = worst_engineers_jobs.head(30)
+    print(f"The job that pays the least is {worst_engineers_jobs}")
+
+
+def highest_paying_quebec(df_sorted):
+    top_quebec_jobs = df_sorted[
+        df_sorted["province"].str.contains("québec", case=False)
+    ]
+    print(f"The job that pays the most in Quebec is {top_quebec_jobs}")
+    return top_quebec_jobs.head(30)
+
+
+# make a function to compare a specific job in a specific province
+def compare_job_province(job, province):
+    df_filtered = df[(df["province"] == province) & (df["job"] == job)]
+    df_grouped = df_filtered.groupby(["job", "region"])["mid_mean"].mean().reset_index()
+    df_sorted = df_grouped.sort_values("mid_mean", ascending=False)
+    return df_sorted
+
+
+def compare_job(job):
+    df_filtered = df[df["job"] == job]
+    df_grouped = df_filtered.groupby(["job", "region"])["mid_mean"].mean().reset_index()
+    df_sorted = df_grouped.sort_values("mid_mean", ascending=False)
+    return df_sorted
+
+
+def compare_province(province):
+    df_filtered = df[df["province"] == province]
+    df_grouped = df_filtered.groupby(["job", "region"])["mid_mean"].mean().reset_index()
+    df_sorted = df_grouped.sort_values("mid_mean", ascending=False)
+    return df_sorted
+
+
+def compare_job_region(job, region):
+    df_filtered = df[(df["region"] == region) & (df["job"] == job)]
+    df_grouped = df_filtered.groupby(["job", "region"])["mid_mean"].mean().reset_index()
+    df_sorted = df_grouped.sort_values("mid_mean", ascending=False)
+    return df_sorted
+
+
+# function to compare a specific job in a all provinces
+def compare_job_all_provinces(job):
+    df_filtered = df[(df["job"] == job)]
+    df_grouped = (
+        df_filtered.groupby(["job", "province"])["mid_mean"].mean().reset_index()
+    )
+    df_sorted = df_grouped.sort_values("mid_mean", ascending=False)
+    return df_sorted
+
+
+# function to find the job with the largest difference, the user can choose between entry, mid or senior
+def find_job_with_biggest_diff(level):
+    # if level is not entry, mid or senior, return an error
+    if level not in ["entry_mean", "mid_mean", "senior_mean"]:
+        return "Error: level must be entry_mean, mid_mean or senior_mean"
+    # Group by the 'job' column and find the difference between the maximum and minimum values of the 'mid' column for each group
+    job_diffs = df.groupby("job")[[level]].apply(lambda x: (x.max() - x.min()).sum())
+
+    # Find the job with the largest overall salary difference
+    max_diff_job = job_diffs.idxmax()
+
+    # Print the result
+    print(
+        "The job with the largest overall salary difference is: {}".format(max_diff_job)
+    )
+
+    return max_diff_job
+
+
+# Detailled breakdown of the salary by province
+def salary_breakdown_by_province(df, job="cloud architect"):
+    # Get the unique provinces
+    provinces = df["province"].unique()
+
+    # Loop over the provinces and compare the job in each province
+    for province in provinces:
+        # Filter the dataframe by province and job
+        df_filtered = df[(df["province"] == province) & (df["job"] == job)]
+
+        # Group the resulting dataframe by job and region, and calculate the mean mid salary
+        df_grouped = (
+            df_filtered.groupby(["job", "region"])["mid_mean"].mean().reset_index()
+        )
+
+        # Sort the grouped dataframe by the mid salary in descending order
+        df_sorted = df_grouped.sort_values("mid_mean", ascending=False)
+
+        # Print the sorted dataframe with the province name
+        print(f"{province.capitalize()}:")
+        print(df_sorted)
+        print()
--- src/pdf.py
+++ src/pdf.py
@@ -161,3 +161,79 @@ print(regions)
 with open("output.txt", "w") as f:
     for item in header_para:
         f.write("%s " % item)
+
+
+# import fitz
+# from operator import itemgetter
+
+# def fonts(doc, granularity=False):
+#     """Extracts fonts and their usage in PDF documents.
+#     :param doc: PDF document to iterate through
+#     :type doc: <class 'fitz.fitz.Document'>
+#     :param granularity: also use 'font', 'flags' and 'color' to discriminate text
+#     :type granularity: bool
+#     :rtype: [(font_size, count), (font_size, count}], dict
+#     :return: most used fonts sorted by count, font style information
+#     """
+#     styles = {}
+#     font_counts = {}
+
+#     for page in doc:
+#         # print(dir(page))
+#         blocks = page.get_text("dict")["blocks"]
+#         for b in blocks:  # iterate through the text blocks
+#             if b["type"] == 0:  # block contains text
+#                 for l in b["lines"]:  # iterate through the text lines
+#                     for s in l["spans"]:  # iterate through the text spans
+#                         if granularity:
+#                             identifier = f"{s['size']}_{s['flags']}_{s['font']}_{s['color']}"
+#                             styles[identifier] = {
+#                                 "size": s["size"],
+#                                 "flags": s["flags"],
+#                                 "font": s["font"],
+#                                 "color": s["color"],
+#                             }
+#                         else:
+#                             identifier = str(s["size"])
+#                             styles[identifier] = {"size": s["size"], "font": s["font"]}
+
+#                         font_counts[identifier] = font_counts.get(identifier, 0) + 1  # count the fonts usage
+
+#     font_counts = sorted(font_counts.items(), key=itemgetter(1), reverse=True)
+
+#     if len(font_counts) < 1:
+#         raise ValueError("Zero discriminating fonts found!")
+
+#     return font_counts, styles
+
+
+# def font_tags(font_counts, styles):
+#     """Returns dictionary with font sizes as keys and tags as value.
+#     :param font_counts: (font_size, count) for all fonts occurring in document
+#     :type font_counts: list
+#     :param styles: all styles found in the document
+#     :type styles: dict
+#     :rtype: dict
+#     :return: all element tags based on font-sizes
+#     """
+#     p_style = styles[font_counts[0][0]]  # get style for most used font by count (paragraph)
+#     p_size = p_style["size"]  # get the paragraph's size
+
+#     # sorting the font sizes high to low, so that we can append the right integer to each tag
+#     font_sizes = sorted(map(float, [font_size for font_size, count in font_counts]), reverse=True)
+
+#     # aggregating the tags for each font size
+#     idx = 0
+#     size_tag = {}
+#     for size in font_sizes:
+#         idx += 1
+#         if size == p_size:
+#             idx = 0
+#             size_tag[size] = "<p>"
+#         if size > p_size:
+#             size_tag[size] = f"<h{idx}>"
+#         elif size < p_size:
+#             size_tag[size] = f"<s{idx}>"
+
+#     return size_tag
+
